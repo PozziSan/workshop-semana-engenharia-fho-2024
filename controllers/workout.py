cf60eb4e2328358b690a26f59893ca78d45a20a8
pozzisan <pedropozzif@gmail.com>
2024-10-22 18:08:51 -0300

    Add Exception Handlers

--- controllers/workout.py
+++ controllers/workout.py
@@ -1,5 +1,7 @@
 from fastapi import APIRouter
+from fastapi.responses import JSONResponse
 
+from exceptions import NotFoundException
 from serializers.workout import Workout, WorkoutCreate
 from models.workout import WorkoutModel
 
@@ -11,16 +13,16 @@ def get_all() -> list[WorkoutModel]:
     return WorkoutModel.all()
 
 @router.post("/", response_model=Workout)
-def create(train: WorkoutCreate) -> WorkoutModel:
-    return WorkoutModel.create(**train.model_dump())
+def create(workout: WorkoutCreate) -> WorkoutModel:
+    return WorkoutModel.create(**workout.model_dump())
 
-@router.delete("/{train_id}")
-def delete(train_id: int):
-    train = WorkoutModel.get_by_id(train_id)
+@router.delete("/{workout_id}")
+def delete(workout_id: int):
+    workout = WorkoutModel.get_by_id(workout_id)
 
-    if not train:
-        return {"status": 404}
+    if not workout:
+        raise NotFoundException()
     
-    train.delete()
+    workout.delete()
 
-    return {"status": 200}
+    return JSONResponse(status_code=200, content={"message": "Success"})
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.responses import JSONResponse
 
 from controllers import health, workout
 from database import Base, engine
@@ -8,3 +9,10 @@ Base.metadata.create_all(engine)
 app = FastAPI()
 app.include_router(health.router)
 app.include_router(workout.router)
+
+@app.exception_handler(Exception)
+def generic_exception_handler():
+    return JSONResponse(
+        status_code=418,
+        content={"message": "Internal Server Error"},
+    )
